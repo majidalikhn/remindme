a1d69d6f48a15e22a33682b6e7c05365bad1e2f5
KhanM@carTrust.de <KhanM>
2022-06-02 21:24:50 +0200

    refactor: refactor EmailDetailsListService name

--- src/main/java/com/mak/remindme/service/EmailDetailsListService.java
+++ src/main/java/com/mak/remindme/service/EmailDetailsListService.java
@@ -17,18 +17,6 @@ public class EmailDetailsListService {
         emailDetails1.setMsgBody("This is Body of First email");
         emailDetailsList.add(emailDetails1);
 
-        EmailDetails emailDetails2 = new EmailDetails();
-        emailDetails2.setSubject("Second email");
-        emailDetails2.setRecipient("majidalikhn@gmail.com");
-        emailDetails2.setMsgBody("This is Body of Second email");
-        emailDetailsList.add(emailDetails2);
-
-        EmailDetails emailDetails3 = new EmailDetails();
-        emailDetails3.setSubject("Third email");
-        emailDetails3.setRecipient("majidalikhn@gmail.com");
-        emailDetails3.setMsgBody("This is Body of Third email");
-        emailDetailsList.add(emailDetails3);
-
         return emailDetailsList;
     }
 
